b10ab19ffdb66839a0feccfcff664d0cc17558c0
Patrick Robert <pro@virtualite.quebec>
2022-01-01 01:54:31 -0500

    Wheels instead of Greetings

--- src/main/java/quebec/virtualite/backend/services/rest/RestServer.java
+++ src/main/java/quebec/virtualite/backend/services/rest/RestServer.java
@@ -17,7 +17,7 @@ import static org.h2.util.StringUtils.isNullOrEmpty;
 @RestController
 public class RestServer
 {
-    private static final Logger log = LoggerFactory.getLogger(RestServer.class);
+    private final Logger log = LoggerFactory.getLogger(RestServer.class);
 
     private final DomainService domainService;
 
