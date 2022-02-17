8de69b5d15ec5c715119284325d12f6db2e935a8
Patrick Robert <pro@virtualite.quebec>
2022-02-16 23:56:38 -0500

    Kotlin

--- src/main/java/quebec/virtualite/backend/Application.kt
+++ src/main/java/quebec/virtualite/backend/Application.kt
@@ -1,31 +1,34 @@
-package quebec.virtualite.backend;
+package quebec.virtualite.backend
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import quebec.virtualite.backend.security.SecurityUserManager;
-import quebec.virtualite.backend.security.SecurityUsers;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import quebec.virtualite.backend.Application
+import quebec.virtualite.backend.security.SecurityUserManager
+import quebec.virtualite.backend.security.SecurityUsers.TEST_PASSWORD
+import quebec.virtualite.backend.security.SecurityUsers.TEST_USER
 
-@SpringBootApplication(scanBasePackages = {"quebec.virtualite.*"})
-@Slf4j
-public class Application
+@SpringBootApplication(scanBasePackages = ["quebec.virtualite.*"])
+open class Application(userManager: SecurityUserManager)
 {
-    static
+    init
     {
-        log.warn("STARTING...");
+        userManager.defineUser(TEST_USER, TEST_PASSWORD)
+
+        // FIXME-0 Implement logging
+//        log.warn("STARTED")
     }
 
-    public Application(SecurityUserManager userManager)
+    companion object
     {
-        userManager.defineUser(
-            SecurityUsers.INSTANCE.getTEST_USER(),
-            SecurityUsers.INSTANCE.getTEST_PASSWORD());
+        init
+        {
+//            Application.log.warn("STARTING...")
+        }
 
-        log.warn("STARTED");
-    }
-
-    public static void main(final String[] args)
-    {
-        SpringApplication.run(Application.class, args);
+        @JvmStatic
+        fun main(args: Array<String>)
+        {
+            SpringApplication.run(Application::class.java, *args)
+        }
     }
 }
