5e536e271b14b232f0aa02e3842994fbb10872f8
Patrick Robert <pro@virtualite.quebec>
2022-02-17 21:23:16 -0500

    Minor change

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE wheels
 (
     id    SERIAL PRIMARY KEY,
-    brand VARCHAR(45) UNIQUE NOT NULL,
+    brand VARCHAR(45)        NOT NULL,
     name  VARCHAR(45) UNIQUE NOT NULL,
 
     UNIQUE (brand, name)
