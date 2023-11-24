99a8b47b855ab2a80f1ebda5119e1845d549ce01
Patrick Robert <pro@virtualite.quebec>
2023-11-24 14:41:51 -0500

    Test copy DTO

--- src/test/java/quebec/virtualite/backend/services/rest/WheelDTOTest.java
+++ src/test/java/quebec/virtualite/backend/services/rest/WheelDTOTest.java
@@ -4,10 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static quebec.virtualite.backend.TestConstants.EMPTY_BRAND;
-import static quebec.virtualite.backend.TestConstants.EMPTY_NAME;
-import static quebec.virtualite.backend.TestConstants.NULL_BRAND;
-import static quebec.virtualite.backend.TestConstants.NULL_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
 import static quebec.virtualite.backend.TestConstants.WHEEL_DTO;
 import static quebec.virtualite.backend.services.utils.TestUtils.validateDTO;
 
@@ -19,27 +16,33 @@ public class WheelDTOTest
     private static final int TWO_ERRORS = 2;
 
     @Test
+    public void copy()
+    {
+        // When
+        WheelDTO result = WHEEL_DTO.copy();
+
+        // Then
+        assertThat(result).isEqualTo(WHEEL_DTO);
+    }
+
+    @Test
     public void validate()
     {
         validateDTO(WHEEL_DTO, NO_ERRORS);
-        validateDTO(
-            new WheelDTO()
-                .setBrand(EMPTY_BRAND)
-                .setName(EMPTY_NAME),
-            TWO_ERRORS);
+        validateDTO(new WheelDTO(), TWO_ERRORS);
     }
 
     @Test
     public void validateBrand()
     {
-        validateDTO(WHEEL_DTO.copy().setBrand(EMPTY_BRAND), ONE_ERROR);
-        validateDTO(WHEEL_DTO.copy().setBrand(NULL_BRAND), ONE_ERROR);
+        validateDTO(WHEEL_DTO.copy().setBrand(null), ONE_ERROR);
+        validateDTO(WHEEL_DTO.copy().setBrand(""), ONE_ERROR);
     }
 
     @Test
     public void validateName()
     {
-        validateDTO(WHEEL_DTO.copy().setName(EMPTY_NAME), ONE_ERROR);
-        validateDTO(WHEEL_DTO.copy().setName(NULL_NAME), ONE_ERROR);
+        validateDTO(WHEEL_DTO.copy().setName(null), ONE_ERROR);
+        validateDTO(WHEEL_DTO.copy().setName(""), ONE_ERROR);
     }
 }
