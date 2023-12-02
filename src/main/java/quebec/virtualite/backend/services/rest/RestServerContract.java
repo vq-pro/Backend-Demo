096a79a428ff510de662eaedfa1f9b82dea8a5dd
Patrick Robert <pro@virtualite.quebec>
2023-12-02 12:51:09 -0500

    Katas

--- src/main/java/quebec/virtualite/backend/services/rest/RestServerContract.java
+++ src/main/java/quebec/virtualite/backend/services/rest/RestServerContract.java
@@ -6,19 +6,28 @@ import java.util.List;
 
 public interface RestServerContract
 {
+    // --------------------------------------------------
     String URL_ADD_WHEEL__PUT = "/wheels";
-    String URL_DELETE_WHEEL = "/wheels/{name}";
-    String URL_GET_WHEEL = "/wheels/{name}";
-    String URL_GET_WHEELS = "/wheels";
-    String URL_UPDATE_WHEEL__POST = "/wheels/{name}";
 
     void addWheel(@Valid WheelDTO wheel);
 
+    // --------------------------------------------------
+    String URL_DELETE_WHEEL = "/wheels/{name}";
+
     void deleteWheel(@NotBlank String name);
+
+    // --------------------------------------------------
+    String URL_GET_WHEEL = "/wheels/{name}";
 
     WheelDTO getWheelDetails(@NotBlank String name);
 
+    // --------------------------------------------------
+    String URL_GET_WHEELS = "/wheels";
+
     List<WheelDTO> getWheelsDetails();
+
+    // --------------------------------------------------
+    String URL_UPDATE_WHEEL__POST = "/wheels/{name}";
 
     void updateWheel(@NotBlank String name, @Valid WheelDTO wheel);
 }
