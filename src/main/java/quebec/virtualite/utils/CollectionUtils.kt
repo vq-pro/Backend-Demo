270b50b487d54d64cef77c29c86230f4aba5c4a7
Patrick Robert <pro@virtualite.quebec>
2023-12-16 18:03:50 -0500

    Development helper utilities

--- src/main/java/quebec/virtualite/utils/CollectionUtils.kt
+++ src/main/java/quebec/virtualite/utils/CollectionUtils.kt
@@ -4,6 +4,34 @@ import java.util.stream.Collectors.toList
 
 object CollectionUtils
 {
+    fun commaSeparatedList(vararg entries: String?): String
+    {
+        return commaSeparatedList(listOf(*entries))
+    }
+
+    fun commaSeparatedList(entries: List<String?>): String
+    {
+        val output = StringBuilder()
+        for (entry in entries)
+        {
+            if (output.length != 0) output.append(", ")
+
+            output.append(entry)
+        }
+
+        return output.toString()
+    }
+
+    fun nameAndBrackets(name: String, brackets: String): String
+    {
+        return "$name ($brackets)"
+    }
+
+    fun nameAndBrackets(name: String, brackets: List<String?>): String
+    {
+        return name + " (" + commaSeparatedList(brackets) + ")"
+    }
+
     fun <A, B> transform(
         items: List<A>,
         lambda: (A) -> B
---
+++ src/main/java/quebec/virtualite/utils/DevUtils.kt
@@ -0,0 +1,14 @@
+package quebec.virtualite.utils
+
+object DevUtils
+{
+    fun IMPLEMENT()
+    {
+        throw AssertionError("Implement this")
+    }
+
+    fun NEXT()
+    {
+        throw AssertionError("GOOD! Next case!")
+    }
+}
