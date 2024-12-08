d94dc93a3b4b963b8835063712dbd3a2cd8bc558
Patrick Robert <pro@virtualite.quebec>
2024-12-08 16:46:32 -0500

    Conversion to cities - Kata Filter By Query

--- src/main/java/quebec/virtualite/backend/services/rest/RestServerContract.java
+++ src/main/java/quebec/virtualite/backend/services/rest/RestServerContract.java
@@ -8,22 +8,25 @@ public interface RestServerContract
 {
     // --------------------------------------------------
     String URL_ADD_CITY__PUT = "/cities";
+
+    void addCity(@Valid CityDTO city);
+
     // --------------------------------------------------
     String URL_DELETE_CITY = "/cities/{name}";
+
+    void deleteCity(@NotBlank String name);
+
     // --------------------------------------------------
     String URL_GET_CITY = "/cities/{name}";
+
+    CityDTO getCityDetails(@NotBlank String name);
+
     // --------------------------------------------------
     String URL_GET_CITIES = "/cities";
     // --------------------------------------------------
     String URL_UPDATE_CITY__POST = "/cities/{name}";
 
-    void addCity(@Valid CityDTO city);
-
-    void deleteCity(@NotBlank String name);
-
     List<CityDTO> getCitiesDetails();
-
-    CityDTO getCityDetails(@NotBlank String name);
 
     void updateCity(@NotBlank String name, @Valid CityDTO city);
 }
