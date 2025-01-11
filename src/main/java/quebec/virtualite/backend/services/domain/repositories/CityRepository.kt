c1e36fcb4ae60666057dc2e9cf2590164cfb1ea9
Patrick Robert <pro@virtualite.quebec>
2025-01-11 16:28:05 -0500

    JPA fixes

--- src/main/java/quebec/virtualite/backend/services/domain/repositories/CityRepository.kt
+++ src/main/java/quebec/virtualite/backend/services/domain/repositories/CityRepository.kt
@@ -1,10 +1,8 @@
 package quebec.virtualite.backend.services.domain.repositories
 
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Component
 import quebec.virtualite.backend.services.domain.entities.CityEntity
 
-@Component
 interface CityRepository : CrudRepository<CityEntity, Long>
 {
     fun deleteByName(name: String)
