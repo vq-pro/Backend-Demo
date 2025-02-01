edb31da22357e6756d6ceefaad702e0afd640348
Patrick Robert <pro@virtualite.quebec>
2025-02-01 12:09:31 -0500

    Utility update to use streams

--- src/it/java/quebec/virtualite/utils/CucumberUtils.kt
+++ src/it/java/quebec/virtualite/utils/CucumberUtils.kt
@@ -15,14 +15,6 @@ object CucumberUtils
 
     fun <T> tableFrom(items: Array<T>, header: List<String>, forEachItem: (T) -> List<String>): DataTable
     {
-        val actual = ArrayList<List<String>>()
-        actual.add(header)
-
-        for (item in items)
-        {
-            actual.add(forEachItem(item))
-        }
-
-        return DataTable.create(actual)
+        return DataTable.create(listOf(header) + items.map(forEachItem))
     }
 }
