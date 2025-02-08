a507f635d6d59db98bd23a8ae3d4cfbc5f5573bd
Patrick Robert <pro@virtualite.quebec>
2025-02-08 17:28:46 -0500

    Allow setting tags at runtime

--- src/it/java/quebec/virtualite/backend/CucumberIT.java
+++ src/it/java/quebec/virtualite/backend/CucumberIT.java
@@ -7,13 +7,11 @@ import org.junit.platform.suite.api.SelectDirectories;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectDirectories(".")
 @SelectClasspathResource("src/features")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @Ignore")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/last-run.html")
 public class CucumberIT
 {
