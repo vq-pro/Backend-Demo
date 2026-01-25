287b272015729f76891a4391cb21e80ee9060567
Patrick Robert <pro@virtualite.quebec>
2026-01-25 14:29:52 -0500

    Versions

--- src/it/java/quebec/virtualite/backend_integration/CucumberIT.java
+++ src/it/java/quebec/virtualite/backend_integration/CucumberIT.java
@@ -1,17 +1,17 @@
-package quebec.virtualite.backend;
+package quebec.virtualite.backend_integration;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.SelectDirectories;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectDirectories(".")
-@SelectClasspathResource("src/features")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/last-run.html")
 public class CucumberIT
 {
